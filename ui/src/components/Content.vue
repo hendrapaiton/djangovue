42013aa68618b5e0293cb3758ac2b2991eadb02e
Hendra Dwi Saputra <hendra@usa.com>
2021-01-08 16:04:05 +0700

    Make draft components (Header, Content and Footer)

---
+++ ui/src/components/Content.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ ui/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
